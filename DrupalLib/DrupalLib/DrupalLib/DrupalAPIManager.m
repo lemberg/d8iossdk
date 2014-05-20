80bc37bb7713617e1af0c3e2fc60a549ee9be232
AleX <alexander.matvienko@lemberg.co.uk>
2014-05-20 16:23:40 +0300

    DrupalAPIManager implementation

--- DrupalLib/DrupalLib/DrupalLib/DrupalAPIManager.m
+++ DrupalLib/DrupalLib/DrupalLib/DrupalAPIManager.m
@@ -7,6 +7,9 @@
 //
 
 #import "DrupalAPIManager.h"
+#import "AFNetworking.h"
+#import "DrupalEntityDeserializer.h"
+#import "DrupalEntitySerializer.h"
 
 static DrupalAPIManager *sharedDrupalAPIManager;
 
@@ -30,11 +33,87 @@ static DrupalAPIManager *sharedDrupalAPIManager;
 }
 
 -(void) postEntity:(DrupalEntity*)entity{
-    //NSString* fullPath = [NSString stringWithContentsOfURL:<#(NSURL *)#> encoding:<#(NSStringEncoding)#> error:<#(NSError *__autoreleasing *)#> baseURL]
+    NSString* fullPath = entity.path;
 }
 
 -(void) getEntity:(DrupalEntity*)entity{
+//    __block dispatch_block_t block = ^(){
+        NSError *blockError;
+        NSString* fullPath = [[self.baseURL absoluteString]stringByAppendingPathComponent: entity.path];
     
+        [AFHTTPRequestOperationManager manager]GET:fullPath parameters:[DrupalEntitySerializer serializeEntity:entity]
+        success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            //
+        }
+        failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//            blockError = error;
+        };
+//        block();
+//    };
+}
+/*- (void)testWithHandler:(void(^)(int result))handler{
+     [obj somemethodwithcompeltionblock:^{
+         int someInt = 10;
+         dispatch_async(dispatch_get_main_queue(), ^{
+             handler(10);
+         });
+     }
+     ];
+ }
+ - (void)callSite{
+     [self testWithHandler:^(int testResult){
+         NSLog(@"Result was %d", testResult);
+     }];
+ }*/
+
+-(void) getEntityWithBlock:(void (^)(DrupalEntity *entity))handler{
+    NSString* fullPath = entity.path;
+    NSURL *url = [NSURL URLWithString:fullPath];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    //HTTP Request
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        //request succeeded
+        dispatch_async(dispatch_get_main_queue(), ^handler{
+            NSLog(@"JSON:%@",(NSString*)responseObject);
+        });
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        //request failed
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Data"
+                                                            message:[error localizedDescription]
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"Ok"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+    }];
+
+//    NSString* fullPath = entity.path;
+//    NSDictionary* params = [NSDictionary dictionary];
+//    //create URL
+//    NSURL *url = [NSURL URLWithString:fullPath];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    //HTP Request
+//    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+//    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+//
+//    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        //request succeeded
+//        NSLog(@"JSON:%@",(NSString*)responseObject);
+//        
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        //request failed
+//        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Data"
+//                                                            message:[error localizedDescription]
+//                                                           delegate:nil
+//                                                  cancelButtonTitle:@"Ok"
+//                                                  otherButtonTitles:nil];
+//        [alertView show];
+//    }];
+//    //
+    [operation start];
 }
 
 @end
